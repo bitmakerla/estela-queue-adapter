868cf496676c65708a03f73da4371a55f42256e0
rodp63 <joaquin.palma@ucsp.edu.pe>
2023-03-19 09:56:37 -0500

    Add direct args support

--- estela_queue_adapter/get_interface.py
+++ estela_queue_adapter/get_interface.py
@@ -15,25 +15,26 @@ def get_queue_env_vars():
     return result
 
 
-def parse_queue_params():
+def parse_queue_args():
     prefix = "QUEUE_PLATFORM_"
     env_vars = [var for var in os.environ if var.startswith(prefix)]
-    params = {var[len(prefix) :].lower(): os.environ[var] for var in env_vars}
-    return params
+    args = {var[len(prefix) :].lower(): os.environ[var] for var in env_vars}
+    return args
 
 
-def get_interface(interfaces):
-    platform, args = os.getenv("QUEUE_PLATFORM"), parse_queue_params()
+def get_interface(interfaces, **args):
+    platform, env_args = os.getenv("QUEUE_PLATFORM"), parse_queue_args()
     if platform not in interfaces:
         raise Exception(
             f"The queuing platform '{platform}' is not currently supported."
         )
-    return interfaces[platform](**args)
+    env_args.update(args)
+    return interfaces[platform](**env_args)
 
 
-def get_producer_interface():
-    return get_interface(producer_interfaces)
+def get_producer_interface(**args):
+    return get_interface(producer_interfaces, **args)
 
 
-def get_consumer_interface():
-    return get_interface(consumer_interfaces)
+def get_consumer_interface(**args):
+    return get_interface(consumer_interfaces, **args)
